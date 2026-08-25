7aab1303742dfad2529a50f280fb9cb6e867f57b
AntonPDC <antonpdecesare@gmail.com>
2026-08-25 13:23:00 -0400

    array functions

--- arrays1.ts
+++ arrays1.ts
@@ -5,3 +5,8 @@ const carMakers = ["ford", "toyota", "chevy"];
 const carmakers2: string[] = [];
 
 const dates = [new Date(), new Date()];
+
+const carByMake = [["f150"], ["corolla"], ["camaro"]];
+
+// Help with inference when extracting values
+const car = carMakers[0];
