45116093400c08b898a3ecd15bf711bb7b5bf918
AntonPDC <antonpdecesare@gmail.com>
2026-08-31 12:32:44 -0400

    tuples

--- tuples.ts
+++ tuples.ts
@@ -12,3 +12,11 @@ const pepsi: [string, boolean, number] = ["brown", true, 40];
 
 // Using the type alias
 const sprite: Drink = ["clear", true, 30];
+const tea: Drink = ["brown", false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+
+const CarStats = {
+  horsepower: 400,
+  weight: 3354,
+};
